93d2c04fdd25ab62efab750d37d65c3d957771ea
Bhushan Rane <bhushan.rane@capgemini.com>
2018-10-23 16:19:53 +0530

    new1

--- gitdemoRelease/ViewController.swift
+++ gitdemoRelease/ViewController.swift
@@ -14,7 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    func new1()
+    {
+        print("new1")
+    }
 
 }
 
