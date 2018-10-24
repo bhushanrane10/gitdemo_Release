d848bbb60ab8b0308dfe755e44792ed86a75fd40
Bhushan Rane <bhushan.rane@capgemini.com>
2018-10-24 13:50:23 +0530

    added ew file

---
+++ gitdemoRelease/fileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  fileViewController.swift
+//  gitdemoRelease
+//
+//  Created by Bhushan Rane on 24/10/18.
+//  Copyright © 2018 Bhushan Rane. All rights reserved.
+//
+
+import UIKit
+
+class fileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
